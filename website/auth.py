7269936dd90947f7520e1e550364ececf630503e
Sahar <sahareyvazzadeh80@gmail.com>
2024-05-18 11:18:46 +0330

    let's start creating web app to learn how authentication works

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,37 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth' , __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods= ['GET','POST'])
 def login():
-    return "<p>LOGIN</p>"
 
-@auth.route('/logout')
+    return render_template("login.html", user=None)
+
+@auth.route('/logout', methods= ['GET', "POST"])
 def logout():
-    return "<p>LOGout</p>"
+    return "<p>Logout</p>"
 
-@auth.route('/signup')
+@auth.route('/signup', methods= ['GET', 'POST'])
 def signup():
-    return "<p>SignUp</p>"
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4 :
+            flash('Email must be greater than 3 characters.', category = 'error')
+        elif len(firstName) < 2:
+            flash('First Name must be greater than 1 characters. ', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match.', category='error')
+        elif password1 < 7:
+            flash('Password must be at least 7 characters.', category='error')
+        else:
+             flash('Account Created!', category='success')
+
+    return render_template("signup.html", user=None)
+
+@auth.route('/home')
+def home():
+    return render_template("home.html", user=None)
