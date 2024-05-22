a97d85b1e4d9242aa976c06a57cce75d2095157c
Sahar <sahareyvazzadeh80@gmail.com>
2024-05-22 18:55:48 +0330

    now time to check for existing users in our database and the related subjects...

--- website/auth.py
+++ website/auth.py
@@ -3,16 +3,34 @@ from . import db
 from .models import User
 # this is going to save the password in a more secure way in which you can not inverse the password(by giving x you'll have y but by giving y you cannot have x
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth' , __name__)
 
 @auth.route('/login', methods= ['GET','POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(email = email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in Successfully!', category='success')
+                login_user(user, remember=True)
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password, try again.', category='error')
+        else:
+            flash('Email does not exist. Sign up first!', category='error')
+
     return render_template("login.html", user=None)
 
-@auth.route('/logout', methods= ['GET', "POST"])
+@auth.route('/logout')
+@login_required
 def logout():
-    return "<p>Logout</p>"
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 @auth.route('/signup', methods= ['GET', 'POST'])
 def signup():
@@ -22,7 +40,10 @@ def signup():
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
-        if len(email) < 4 :
+        user = User.query.filter_by(email=email).first()
+        if user:
+            flash('Email already exists.', category='error')
+        elif len(email) < 4 :
             flash('Email must be greater than 3 characters.', category = 'error')
         elif len(first_name) < 2:
             flash('First Name must be greater than 1 characters. ', category='error')
